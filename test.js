89464bf773808023fc2e7882d47c9c8785a5a964
DungvNg <nvdung694@gmail.com>
2019-09-30 18:00:35 +0700

    [Common] 2nd tutorial of html basic
    Description:
      -
    Affected file:
      -

---
+++ test.js
@@ -0,0 +1,9 @@
+var myVar = "hellooooo";
+function bt_button1_onclick()
+{
+  alert("asda");
+}
+var p = document.getElementById("some-paragraph");
+p.addEventListener("click", function(event) {
+  alert("alksd");
+});
